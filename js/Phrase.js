81ebbd7e11c0c31521e50f198a5d80703308a9a5
Matt Hawes <73285477+Matt-J-Hawes@users.noreply.github.com>
2020-11-14 18:23:13 +0000

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -4,53 +4,48 @@
  * Phrase.js */
 
 
- class Phrase{
+class Phrase{
 
-	 	constructor(phrase){
-	 		this.phrase = phrase.toLowerCase() }
-	 	
-		/*******************************************************
-		* Display phrase on game board
-		********************************************************/
+	constructor(phrase){
+		this.phrase = phrase.toLowerCase() }
 
-		addPhraseToDisplay(){
-		 const ul = document.querySelector("ul")
-		 const splitLetter = this.phrase.split('')
-			for(let char of splitLetter){
-			if(char !== ' '){
-				let letterLi = document.createElement('li')
-				letterLi.innerHTML = char
-				letterLi.classList.add("letter")
-				letterLi.classList.add("hide")
-				ul.appendChild(letterLi) }				
-			else {
-				let spaceLi = document.createElement('li')
-				spaceLi.innerHTML = char
-				spaceLi.className = "space"
-				ul.appendChild(spaceLi)
-				}
-			}
-			return splitLetter
-		}
+/*******************************************************
+* Display phrase on game board
+********************************************************/
 
-	    /********************************************************
-		* Checks if passed letter is in phrase
-		* @param (string) letter - Letter to check
-		*********************************************************/
-
-		checkLetter(letter){        
-		    return this.phrase.includes(letter) }			 
+	addPhraseToDisplay(){
+		const ul = document.querySelector("ul")
+		const splitLetter = this.phrase.split('')
+		for(let char of splitLetter){
+		if(char !== ' '){
+		let letterLi = document.createElement('li')
+		letterLi.innerHTML = char
+		letterLi.classList.add("letter")
+		letterLi.classList.add("hide")
+		ul.appendChild(letterLi) }				
+		else {
+		let spaceLi = document.createElement('li')
+		spaceLi.innerHTML = char
+		spaceLi.className = "space"
+		ul.appendChild(spaceLi) } }			
+		return splitLetter }
 		
-        /*********************************************************
-		* Displays passed letter on screen after a match is found
-		* @param (string) letter - Letter to display
-		**********************************************************/
+/********************************************************
+* Checks if passed letter is in phrase
+* @param (string) letter - Letter to check
+*********************************************************/
 
-		showMatchedLetter(letter){
-				const li = document.querySelectorAll('li.hide.letter')
-				li.forEach(phrase => {
-				if(letter === phrase.textContent.toLowerCase()){
-				phrase.classList.replace("hide", "show") }})
-											
-			}
-		}
+	checkLetter(letter){        
+		return this.phrase.includes(letter) }			 
+
+/*********************************************************
+* Displays passed letter on screen after a match is found
+* @param (string) letter - Letter to display
+**********************************************************/
+
+	showMatchedLetter(letter){
+		const li = document.querySelectorAll('li.hide.letter')
+		li.forEach(phrase => {
+		if(letter === phrase.textContent.toLowerCase()){
+		phrase.classList.replace("hide", "show") }}) } }
+		
